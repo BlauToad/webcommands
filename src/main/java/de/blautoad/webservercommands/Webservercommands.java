1d19881bcce2ca670512743bf60d490fb16e194c
BlauToad <53124247+BlauToad@users.noreply.github.com>
2023-02-24 20:13:21 +0100

    Add files via upload

---
+++ src/main/java/de/blautoad/webservercommands/Webservercommands.java
@@ -0,0 +1,11 @@
+package de.blautoad.webservercommands;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Webservercommands implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
